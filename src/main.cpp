da62b725525bc2c92f14aa21e648a8a0174e959f
Jesus Guio <jfranciscoguio@gmail.com>
2020-04-04 20:44:48 -0500

    Draw a triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,117 @@
 #include <iostream>
+#include <cstring>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
+
+GLuint VAO, VBO, shader;
+
+// Vertex Shader
+static const char* vShader = "                                                \n\
+#version 330                                                                  \n\
+                                                                              \n\
+layout (location = 0) in vec3 pos;											  \n\
+                                                                              \n\
+void main()                                                                   \n\
+{                                                                             \n\
+    gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);				  \n\
+}";
+
+// Fragment Shader
+static const char* fShader = "                                                \n\
+#version 330                                                                  \n\
+                                                                              \n\
+out vec4 colour;                                                              \n\
+                                                                              \n\
+void main()                                                                   \n\
+{                                                                             \n\
+    colour = vec4(1.0, 0.0, 0.0, 1.0);                                        \n\
+}";
+
+
+void createTriangle() {
+    GLfloat vertices[] {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f
+    };
+
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+}
+
+void addShader(GLuint _program, const char* _shaderCode, GLenum _shaderType) {
+    GLuint newShader = glCreateShader(_shaderType);
+
+    const GLchar* shaderCode[1];
+    shaderCode[0] = _shaderCode;
+
+    GLint codeLenght[1];
+    codeLenght[0] = strlen(_shaderCode);
+
+    glShaderSource(newShader, 1, shaderCode, codeLenght);
+    glCompileShader(newShader);
+
+    GLint result = 0;
+    GLchar eLog[1024] = { 0 };
+
+    glGetShaderiv(newShader, GL_COMPILE_STATUS, &result);
+
+    if ( !result ) {
+        glGetShaderInfoLog(shader, sizeof(eLog), nullptr, eLog);
+        std::cerr << "Error compiling " << _shaderType << " shader: " <<  eLog << std::endl;
+        return ;
+    }
+
+    glAttachShader(_program, newShader);
+}
+
+void compileShader() {
+    shader = glCreateProgram();
+
+    if ( !shader ) {
+        std::cerr << "Error creating shader program" << std::endl;
+        return ;
+    }
+
+    addShader(shader, vShader, GL_VERTEX_SHADER);
+    addShader(shader, fShader, GL_FRAGMENT_SHADER);
+
+    GLint result = 0;
+    GLchar eLog[1024] = { 0 };
+
+    glLinkProgram(shader);
+    glGetProgramiv(shader, GL_LINK_STATUS, &result);
+
+    if ( !result ) {
+        glGetProgramInfoLog(shader, sizeof(eLog), nullptr, eLog);
+        std::cerr << "Error linking program: " << eLog << std::endl;
+        return ;
+    }
+
+    glValidateProgram(shader);
+    glGetProgramiv(shader, GL_VALIDATE_STATUS, &result);
+
+    if ( !result ) {
+        glGetProgramInfoLog(shader, sizeof(eLog), nullptr, eLog);
+        std::cerr << "Error validating program: " << eLog << std::endl;
+        return ;
+    }
+}
 
 int main() {
     // Initialise GLFW
@@ -53,14 +160,25 @@ int main() {
     // Setup Viewport size
     glViewport(0, 0, bufferWidth, bufferHeight);
 
+    createTriangle();
+    compileShader();
+
     // Loop until window closed
     while ( !glfwWindowShouldClose(mainWindow) ) {
         // Get + Handle user input
         glfwPollEvents();
 
         // Clear Window
-        glClearColor(1, 0, 0, 1);
+        glClearColor(0, 0, 0, 1);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader);
+
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
+        glUseProgram(0);
 
         glfwSwapBuffers(mainWindow);
     }
