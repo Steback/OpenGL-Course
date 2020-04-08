029f8034bc104d417b447cd251f799f02f59ea69
Jesus Guio <jfranciscoguio@gmail.com>
2020-04-07 19:11:07 -0500

    Add GLM lib

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,14 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include "../lib/glm/glm.hpp"
+#include "../lib/glm/gtc/matrix_transform.hpp"
+#include "../lib/glm/gtc/type_ptr.hpp"
+
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
 
-GLuint VAO, VBO, shader, uniformXMove;
+GLuint VAO, VBO, shader, uniformModel;
 
 bool direction = true;
 float triOffset  = 0.0f;
@@ -20,11 +24,11 @@ static const char* vShader = "                                                \n
 #version 450                                                                  \n\
                                                                               \n\
 layout (location = 0) in vec3 pos;											  \n\
-uniform float xMove;											              \n\
+uniform mat4 model;											                  \n\
                                                                               \n\
 void main()                                                                   \n\
 {                                                                             \n\
-    gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, pos.z, 1.0);				  \n\
+    gl_Position = model * vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);				  \n\
 }";
 
 // Fragment Shader
@@ -35,7 +39,7 @@ out vec4 colour;                                                              \n
                                                                               \n\
 void main()                                                                   \n\
 {                                                                             \n\
-    colour = vec4(1.0, 0.0, 0.0, 1.0);                                        \n\
+    colour = vec4(0.0, 1.0, 0.0, 1.0);                                        \n\
 }";
 
 
@@ -119,7 +123,7 @@ void compileShader() {
         return ;
     }
 
-    uniformXMove = glGetUniformLocation(shader, "xMove");
+    uniformModel = glGetUniformLocation(shader, "model");
 }
 
 int main() {
@@ -193,7 +197,10 @@ int main() {
 
         glUseProgram(shader);
 
-        glUniform1f(uniformXMove, triOffset);
+        glm::mat4 model(1.0f);
+        model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
+
+        glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
